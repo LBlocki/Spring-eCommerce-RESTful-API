004110dbf812f39b0f86308693c6c3b7e4fb3733
Łukasz Błocki <lukolin21@gmail.com>
2019-08-19 09:12:06 +0200

    Initial commit

---
+++ src/main/java/com/blocki/springrestonlinestore/SpringRestOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.blocki.springrestonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestOnlineStoreApplication.class, args);
+	}
+
+}
