8d6b6757fea77717fc59ac244ac503d9ae43fba8
Łukasz Błocki <lukolin21@gmail.com>
2019-08-20 19:20:33 +0200

    Adding mappings. Objects with different name required specific mapping (DTO to domain and back). Lack of those caused null exception for those objects after conversion.

--- src/main/java/com/blocki/springrestonlinestore/api/v1/mapper/ShoppingCartItemMapper.java
+++ src/main/java/com/blocki/springrestonlinestore/api/v1/mapper/ShoppingCartItemMapper.java
@@ -3,14 +3,20 @@ package com.blocki.springrestonlinestore.api.v1.mapper;
 import com.blocki.springrestonlinestore.api.v1.model.ShoppingCartItemDTO;
 import com.blocki.springrestonlinestore.core.domain.ShoppingCartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ShoppingCartItemMapper {
 
     ShoppingCartItemMapper INSTANCE = Mappers.getMapper(ShoppingCartItemMapper.class);
 
+    @Mapping(source = "product", target = "productDTO")
+    @Mapping(source = "shoppingCart", target = "shoppingCartDTO")
     ShoppingCartItemDTO ShoppingCartItemToShoppingCartItemDTO(ShoppingCartItem shoppingCartItem);
 
+    @Mapping(source = "productDTO", target = "product")
+    @Mapping(source = "shoppingCartDTO", target = "shoppingCart")
     ShoppingCartItem ShoppingCartItemDTOToShoppingCartItem(ShoppingCartItemDTO shoppingCartItemDTO);
 }
--- src/main/java/com/blocki/springrestonlinestore/api/v1/mapper/ShoppingCartMapper.java
+++ src/main/java/com/blocki/springrestonlinestore/api/v1/mapper/ShoppingCartMapper.java
@@ -3,14 +3,20 @@ package com.blocki.springrestonlinestore.api.v1.mapper;
 import com.blocki.springrestonlinestore.api.v1.model.ShoppingCartDTO;
 import com.blocki.springrestonlinestore.core.domain.ShoppingCart;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ShoppingCartMapper {
 
     ShoppingCartMapper INSTANCE = Mappers.getMapper(ShoppingCartMapper.class);
 
+    @Mapping(source = "user", target = "userDTO")
+    @Mapping(source = "shoppingCartItems", target = "shoppingCartItemDTOs")
     ShoppingCartDTO shoppingCartToShoppingCartDTO(ShoppingCart shoppingCart);
 
+    @Mapping(source = "userDTO", target = "user")
+    @Mapping(source = "shoppingCartItemDTOs", target = "shoppingCartItems")
     ShoppingCart shoppingCartDTOToShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
