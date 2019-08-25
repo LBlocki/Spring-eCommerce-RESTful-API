7731b4d127fe9d6d46e8ffae6c0da700d5fb7d26
Łukasz Błocki <lukolin21@gmail.com>
2019-08-25 22:50:17 +0200

    Adding Validation handler and refactoring to use ResponseEntities

--- src/main/java/com/blocki/springrestonlinestore/api/v1/controllers/ExceptionController.java
+++ src/main/java/com/blocki/springrestonlinestore/api/v1/controllers/ExceptionController.java
@@ -1,34 +1,76 @@
 package com.blocki.springrestonlinestore.api.v1.controllers;
 
+import com.blocki.springrestonlinestore.core.exceptions.CustomErrorResponse;
 import com.blocki.springrestonlinestore.core.exceptions.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolationException;
-import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public void handleNotFoundException(Exception exception) {
+    public ResponseEntity<CustomErrorResponse> handleNotFoundException(Exception exception) {
 
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, exception.getMessage(), exception);
+       log.debug("Resource Not found exception caught");
+
+       CustomErrorResponse errors = new CustomErrorResponse();
+       errors.setTimestamp(LocalDateTime.now());
+       errors.setError(exception.getMessage());
+       errors.setStatus(HttpStatus.NOT_FOUND.value());
+
+       return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NumberFormatException.class)
-    public void handleNumberFormatException(Exception exception) {
+    public ResponseEntity<CustomErrorResponse> handleNumberFormatException(Exception exception) {
 
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage(), exception);
+        log.debug("Number format exception caught");
+
+        CustomErrorResponse errors = new CustomErrorResponse();
+        errors.setTimestamp(LocalDateTime.now());
+        errors.setError(exception.getMessage());
+        errors.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public void constraintViolationException(HttpServletResponse response) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value());
+    @Override
+    public ResponseEntity<Object>  handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                HttpHeaders headers, HttpStatus status,
+                                                                WebRequest request) {
+
+        log.debug("Validation failed");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        //Get all fields errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
     }
 
 }
