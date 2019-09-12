26044272d0fc1ba914d0c3de0aa5d9048c85d91f
Łukasz Błocki <lukolin21@gmail.com>
2019-09-12 11:37:03 +0200

    Adding category mvc testing. Required adding @Expose Resource for in the controller and removing blank spaces in resource assembler to make custom message converter ( hal is not automatically configured when standalone setup is used. Reference #71

--- src/main/java/com/blocki/springrestonlinestore/api/v1/controllers/CategoryController.java
+++ src/main/java/com/blocki/springrestonlinestore/api/v1/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import com.blocki.springrestonlinestore.api.v1.models.CategoryDTO;
 import com.blocki.springrestonlinestore.core.services.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@ExposesResourceFor(value = CategoryDTO.class)
 @RequestMapping(value = CategoryController.CATEGORIES_BASIC_URL, produces = "application/hal+json")
 public class CategoryController {
 
